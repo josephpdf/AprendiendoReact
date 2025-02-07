b609252e45aa7673b350959f76572e1fc496734d
JosephPDF <josephpf12@hotmail.com>
2025-02-07 15:08:59 -0600

    Hook with useState

--- my-app/src/components/Welcome.js
+++ my-app/src/components/Welcome.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 export default function Welcome(props) {
-  console.log(props);
+  const [counter, setCounter] = useState(0);
   const { message, name } = props;
   return (
     <div>
       <h1>Hola, {name}</h1>
-      <p>{message}</p>
+      <h2>Contador de react con Hooks</h2>
+      <h4>El número del contador es: {counter}</h4>
+      <button type="submit" onClick={() => setCounter(counter + 1)}>
+        Sumar contador
+      </button>
     </div>
   );
 }
