12a3680b3aafb3e5c36f7b030e73def63ed339ec
JosephPDF <josephpf12@hotmail.com>
2025-02-08 11:49:59 -0600

    Hook with useEffect

--- my-app/src/components/Welcome.js
+++ my-app/src/components/Welcome.js
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Welcome(props) {
   const [counter, setCounter] = useState(0);
+  const [semaforo, setSemaforo] = useState(false);
   const { message, name } = props;
+
+  useEffect(() => {
+    console.log(semaforo);
+  }, [semaforo]);
+  
+  const contar = () => {
+    setCounter(counter + 1);
+    setSemaforo(!semaforo);
+  }
+
   return (
     <div>
       <h1>Hola, {name}</h1>
       <h2>Contador de react con Hooks</h2>
       <h4>El número del contador es: {counter}</h4>
-      <button type="submit" onClick={() => setCounter(counter + 1)}>
+      <p>El semáforo está en color {semaforo ? "red" : "green"}</p>
+      <button type="submit" onClick={(contar)}>
         Sumar contador
       </button>
+      <p>{message}</p>
     </div>
   );
 }
