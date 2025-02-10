aa15e12f6c4979e9395d3f98a62a7c55967adc41
JosephPDF <josephpf12@hotmail.com>
2025-02-10 17:43:59 -0600

    useState with data

--- rick-morty/src/App.js
+++ rick-morty/src/App.js
@@ -1,18 +1,27 @@
 import imageRickMorty from './img/rick-morty.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
 
-  const reqApi = () => {
-    console.log("Clicking");
-  }
+  const reqApi = async() => {
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+       
+    setCharacters(characterApi.results);
+  };
 
+  console.log(characters);
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
         <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
-        <button onClick={reqApi} calassName="btn-search">Buscar Personajes</button>
+        <button onClick={reqApi} calassName="btn-search">
+          Buscar Personajes
+        </button>
       </header>
     </div>
   );
