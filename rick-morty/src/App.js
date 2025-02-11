f8f9a6eebceafcf61c499b849e324925bdaa548f
JosephPDF <josephpf12@hotmail.com>
2025-02-10 20:28:10 -0600

    New component

--- rick-morty/src/App.js
+++ rick-morty/src/App.js
@@ -1,27 +1,32 @@
-import imageRickMorty from './img/rick-morty.png';
-import './App.css';
-import { useState } from 'react';
+import imageRickMorty from "./img/rick-morty.png";
+import "./App.css";
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
   const [characters, setCharacters] = useState(null);
 
-  const reqApi = async() => {
-    const api = await fetch('https://rickandmortyapi.com/api/character');
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
     const characterApi = await api.json();
-       
+
     setCharacters(characterApi.results);
   };
 
-  console.log(characters);
-  
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
-        <button onClick={reqApi} calassName="btn-search">
-          Buscar Personajes
-        </button>
+        {characters ? (
+          <Characters characters={characters} />
+        ) : (
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} calassName="btn-search">
+              Buscar Personajes
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
---
+++ rick-morty/src/components/Characters.js
@@ -0,0 +1,7 @@
+export default function Characters(props) {
+    console.log(props);
+    
+    return <div>
+        <h1>Desde Characters.JS</h1>
+    </div>
+}
