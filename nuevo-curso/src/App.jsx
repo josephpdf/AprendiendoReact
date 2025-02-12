713ec4f6256e2b815a6f86c1e7b534ee6dd93200
JosephPDF <josephpf12@hotmail.com>
2025-02-12 14:18:10 -0600

    Primer componente

--- nuevo-curso/src/App.jsx
+++ nuevo-curso/src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import PrimerComponente from './components/PrimerComponente';
 
 function App() {
   return (
@@ -7,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola mundo!
+          <PrimerComponente/>
         </p>
         <a
           className="App-link"
---
+++ nuevo-curso/src/components/PrimerComponente.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class PrimerComponente extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (
+            <>
+            <h1>Primer componente</h1>
+            </>
+        );
+    }
+}
+ 
+export default PrimerComponente;
